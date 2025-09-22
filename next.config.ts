ab81b64e4f6747a18892b4d7981fbe525bce0b48
saroobidy10289 <sarobidyf036@gmail.com>
2025-09-22 09:40:34 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Configuration pour l'export statique
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  
   eslint: {
     // Disable ESLint during builds to prevent unescaped entity errors
     ignoreDuringBuilds: true,
